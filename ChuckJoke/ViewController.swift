f17be7b2b12778acb1ab41450457ac706b9eab8c
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2023-09-26 14:33:33 -0300

    Initial Commit

---
+++ ChuckJoke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckJoke
+//
+//  Created by Daniel Washington Ignacio on 26/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
